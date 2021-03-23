8ba1a5aaee90edb65d08155b0404679761306abe
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2021-03-23 22:58:00 +0300

    no message

--- app.config.js
+++ app.config.js
@@ -3,8 +3,9 @@ module.exports = {
         name: "app",
         error_file : "/var/log/pm2_err.log",
         out_file : "/var/log/pm2_out.log",
-        script: "./app.js",
-        watch: true,
+        script: "npm",
+        watch: false,
+        args: "start",
         cwd: "/var/SiteBuilderWebsocket/",
         ignore_watch: [
             "package-lock.json",
